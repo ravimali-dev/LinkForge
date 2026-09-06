5c332e9923783051bec580643c4c350d620df143
Ravi Mali <ravimalakar091@gmail.com>
2026-09-06 12:43:59 +0530

    add Url model with schema for original URL and short code

---
+++ models/url.models.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const urlSchema = new mongoose.Schema({
+  originalUrl: {
+    type: String,
+    required: true
+  },
+  shortCode: {
+    type: String,
+    required: true,
+    unique: true
+  }
+});
+
+const Url = mongoose.model('Url', urlSchema);
+
+export default Url;
