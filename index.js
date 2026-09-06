a3a5721d7d53926c70c0ea63fa5db607102ca36d
Ravi Mali <ravimalakar091@gmail.com>
2026-09-06 13:26:48 +0530

    update server port to 8001 and refine URL routing structure

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 import express from "express";
 import connectDB from './config/db.js'
 import dotenv from "dotenv";
-import urlRouter from "./routes/url.routes.js"
+import urlRouter from "./routes/url.routes.js";
+import {showUrl} from './controllers/url.controllers.js'
 
 dotenv.config();
 
 const app = express();
-const port = 8000;
+const port = 8001;
 app.use(express.json());
 
-app.use('/url',urlRouter)
+app.use('/api/url', urlRouter); 
+app.get('/:shortCode', showUrl);
 
 // index.js
 connectDB().then(() => {
