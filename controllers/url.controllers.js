909a78b4f19bad4547bb4c85814bcb558c542101
Ravi Mali <ravimalakar091@gmail.com>
2026-09-06 13:26:41 +0530

    refactor getUrl response format and add showUrl controller for redirecting short URLs

--- controllers/url.controllers.js
+++ controllers/url.controllers.js
@@ -9,10 +9,23 @@ const getUrl = async (req, res) => {
     }
     const shortCode = nanoid(6);
     const url = await Url.create({ originalUrl, shortCode });
-    res.status(201).json(shortCode);
+    res.status(201).json({ shortCode: shortCode });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 };
 
-export { getUrl };
+const showUrl = async (req, res) => {
+  try {
+    const { shortCode } = req.params;
+    const url = await Url.findOne({ shortCode });
+    if (!url) {
+      return res.status(404).json({ error: "Short URL not found" });
+    }
+
+    res.redirect(url.originalUrl);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+export { getUrl, showUrl };
