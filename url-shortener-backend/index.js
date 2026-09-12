8e1b37844433f43b81e17ac7170744df1e952530
Ravi Mali <ravimalakar091@gmail.com>
2026-09-12 11:40:18 +0530

    Add redirect and URL routes with rate limiting
    - Created redirect.routes.js to handle URL redirection based on short codes.
    - Implemented url.routes.js to manage URL creation and analytics retrieval.
    - Added rate limiting to the URL creation endpoint to prevent abuse.

--- url-shortener-backend/index.js
+++ url-shortener-backend/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from 'cors';
 import connectDB from './config/db.js';
 import dotenv from "dotenv";
 import urlRouter from "./routes/url.routes.js";
@@ -9,6 +10,12 @@ dotenv.config();
 const app = express();
 const port = 8001;
 app.use(express.json());
+
+
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true   // baad mein jab JWT cookies add karoge, tab isse zaroorat padegi
+}));
 
 app.use('/api/url', urlRouter);
 app.use('/', redirectRouter);
--- url-shortener-backend/routes/url.routes.js
+++ url-shortener-backend/routes/url.routes.js
@@ -8,7 +8,7 @@ const limiter = rateLimit({
     windowMs: 15 * 60 * 1000,
     max: 100,
     message: "Too many requests, please try again later"
-});
+}); 
 
 router.post('/', limiter, getUrl);
 router.get('/analytics/:shortCode', getAnalytics);
