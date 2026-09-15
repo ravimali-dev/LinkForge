98bfe7368a8630a5d7c14f0bc77da979c87e23c8
Ravi Mali <ravimalakar091@gmail.com>
2026-09-15 22:03:59 +0530

    Add refresh-token route to auth routes and standardize route definitions

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import {registerUser, loginUser, logoutUser} from '../controllers/user.controller.js'
-import {verifyJWT} from '../middlewares/auth.middleware.js'
+import { registerUser, loginUser, logoutUser, refreshAccessToken } from "../controllers/user.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
-router.post('/register',registerUser)
-router.post('/login', loginUser);
-router.post('/logout',verifyJWT, logoutUser )
+router.route("/register").post(registerUser);
+router.route("/login").post(loginUser);
+router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/refresh-token").post(refreshAccessToken);
 
 export default router;
